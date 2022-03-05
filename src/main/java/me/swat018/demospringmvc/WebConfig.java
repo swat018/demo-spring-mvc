110d5aab067980ae1082fce0572900094c36abfa
jinwoo park <swat018@gmail.com>
2022-03-05 19:08:10 +0900

    feat : 스프링 부트의 스프링 MVC 설정

---
+++ src/main/java/me/swat018/demospringmvc/WebConfig.java
@@ -0,0 +1,14 @@
+package me.swat018.demospringmvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.format.FormatterRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addFormatters(FormatterRegistry registry) {
+        WebMvcConfigurer.super.addFormatters(registry);
+    }
+}
